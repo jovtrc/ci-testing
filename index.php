9c26f3c940a37916a3b386c2fc7218ea21f80b22
João Carvalho <jovtrc@gmail.com>
2023-07-31 13:24:30 -0300

    teste do PHPCS

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * Exemplo de documentação
+ *
+ * Exemplo de descrição
+ *
+ * @link https://developer.wordpress.org/coding-standards/inline-documentation-standards/php/#6-file-headers
+ *
+ * @package Testing
+ * @subpackage Component
+ * @since 1.0
+ */
+
+require 'vendor/autoload.php';
+
+
+function myTest($abc) {
+	if("xyz" == $abc) {
+		echo "XYZ";
+	} else {
+		echo $abc;
+	}
+}
