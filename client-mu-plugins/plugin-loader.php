0e2a8433c78bed177b69ac4f40cd6a81bd48e40c
João Carvalho <jovtrc@gmail.com>
2023-08-19 13:21:41 -0300

    ignora pasta vendor do composer no repositório

--- client-mu-plugins/plugin-loader.php
+++ client-mu-plugins/plugin-loader.php
@@ -1,15 +1,4 @@
 <?php
-/**
- * VIP recommends loading all plugins for your site in code. Loading plugins
- * through code results in more control and greater consistency across
- * development environments. Using this file to do so helps load and activate
- * plugins as early as possible in the WordPress load order.
- *
- * @see https://docs.wpvip.com/how-tos/activate-plugins-through-code/
- * @see https://docs.wpvip.com/technical-references/vip-codebase/client-mu-plugins-directory/
- */
-
-// wpcom_vip_load_plugin;
 
 $autoloadFile = __DIR__ . '/vendor/autoload.php';
 
