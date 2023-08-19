db399c149d99a254c4b32ddc769fff83e7e6e0d7
João Carvalho <jovtrc@gmail.com>
2023-08-19 13:19:48 -0300

    remove vendor do composer

--- client-mu-plugins/plugin-loader.php
+++ client-mu-plugins/plugin-loader.php
@@ -11,6 +11,14 @@
 
 // wpcom_vip_load_plugin;
 
+$autoloadFile = __DIR__ . '/vendor/autoload.php';
+
+if ( file_exists( $autoloadFile ) ) {
+	require $autoloadFile;
+}
+
+
+
 /**
  * The above example requires the plugin to use a specific naming structure: /plugin-name/plugin-name.php
  * You can also specify a plugin's root file:
